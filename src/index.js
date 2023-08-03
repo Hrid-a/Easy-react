b5ec539779df5148f567f8dac359825f267a0ab7
Ahmed Hrid <ahmedhridw@gmail.com>
2023-08-03 19:39:07 +0100

    Learn react the right way

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const heading = <h1>This not an html code nor JavaScript Code this JSX code</h1>;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
