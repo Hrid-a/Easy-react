20d201fd1e8962ba71ce0da742061d4a055226c3
Ahmed Hrid <ahmedhridw@gmail.com>
2023-09-29 12:14:53 +0100

    remove duplicated directory

--- src/utils/cons.js
+++
@@ -1,4 +0,0 @@
-export const CDN_URL = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.47iguDl7iyEqOZZTobaKegHaEK%26pid%3DApi&f=1&ipt=6e5097d42e2e74e07b785224f3e0cd5dab008c884bea7ccabf5aea946a5906a9&ipo=images"
-export const RES_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
-export const ISSUES_URL = "https://www.swiggy.com/dapi/support/issues/"
-// Learning Hooks
